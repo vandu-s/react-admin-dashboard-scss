3287894f7edce0d907f516cf91240fe626f77cae
vandu-s <vandana.singandipe@gmail.com>
2023-03-03 17:11:34 +0530

    widget component done

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,7 @@
+import "./chart.scss";
+
+const Chart = () => {
+  return <div>Chart</div>;
+};
+
+export default Chart;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
+import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
@@ -9,7 +12,16 @@ const Home = () => {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        Home Container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   );
